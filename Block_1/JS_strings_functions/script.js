166d5ef0226ff39a0c038c656a8395b9c5c00d73
AlexandraMH <alexandramh1001@gmail.com>
2024-05-21 12:37:38 +0100

    lab JS functions and strings bonus done already

--- Block_1/JS_strings_functions/script.js
+++ Block_1/JS_strings_functions/script.js
@@ -31,10 +31,9 @@
   //Iteration 3
   
   function highAndLow(string) {
-    let arrayOfNumbers = string.split(' ')
-    let highest = Math.max(...arrayOfNumbers)
-    let lowest = Math.min(...arrayOfNumbers)
-    return highest + ' ' + lowest
+    let max = Math.max(...string.split(' '))
+    let min = Math.min(...string.split(' '))
+    return max + ' ' + min
   }
 
   console.log(highAndLow("1 2 3 4 5"))
@@ -50,28 +49,4 @@
   console.log(reverse("Hello      World"))
   console.log(reverse('Hi    There.'))
   
-  //Bonus
-  
-  //Iteration 1
-  
-  //Iteration 1.1
- /* function rmvFirstAndLastChar(string) {
-    return string.slice(1,-1)
-  }
-  
-  //Iteration 2
-  function nickn(name){
-    if (name.length < 4) return 'Error: Name too short'
-    if (name[2] === 'a' || name[2] === 'e' || name[2] === 'i' || name[2] === 'o' || name[2] === 'u' ) return name.slice(0, 4) //coge desde la primera letra hasta la cuarta pero sin coger la última
-    else return name.slice(0,3)
-  }
-  
-  //Iteration 3
-  
-  function maxAndMin(string) {
-    let max = Math.max(...string.split(' '))
-    let min = Math.min(...string.split(' '))
-    return max + ' ' + min
-  }
-  const result12 = maxAndMin("1 9 3 4 -5")
-  console.log(result12) */
+  //Bonus done
